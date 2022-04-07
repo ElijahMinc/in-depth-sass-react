ecd7f9588717d53f8df84b516f7fb59869f17c87
ilya protsenko <ilya.protsenko@cyper.ca>
2022-04-07 11:59:39 +0300

    Deleted unnecessary files, install sass  practicdependencies, practice with sassass/At-rules, practice with sass/@mixin-s, sass/@content, sass/meta.keywords, sass/nth(),sass/@eac), sass/@for, sass/@each, sass/@if,sass/multiple @content

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <button className="btn sm">
+          <span className="btn-title">Button</span>
+        </button>
+        <button className="btn md">
+          <span className="btn-title">Button</span>
+        </button>
+        <button className="btn lg">
+          <span className="btn-title">Button</span>
+        </button>
+
+
+        <button className="btn sm btn-type-success">
+          <span className="btn-title">Button</span>
+        </button>
+        <button className="btn md btn-type-error" disabled>
+          <span className="btn-title">Button</span>
+        </button>
+        <button className="btn lg btn-type-info">
+          <span className="btn-title">Button</span>
+        </button>
+    </div> 
   );
 }
 
