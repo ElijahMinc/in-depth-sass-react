193815d73317da105facdd0fbaf801915bd28230
ilya protsenko <ilya.protsenko@cyper.ca>
2022-04-07 14:30:38 +0300

    worked on with sasss interpolation

--- src/App.js
+++ src/App.js
@@ -2,26 +2,7 @@
 function App() {
   return (
     <div className="App">
-        <button className="btn sm">
-          <span className="btn-title">Button</span>
-        </button>
-        <button className="btn md">
-          <span className="btn-title">Button</span>
-        </button>
-        <button className="btn lg">
-          <span className="btn-title">Button</span>
-        </button>
-
-
-        <button className="btn sm btn-type-success">
-          <span className="btn-title">Button</span>
-        </button>
-        <button className="btn md btn-type-error" disabled>
-          <span className="btn-title">Button</span>
-        </button>
-        <button className="btn lg btn-type-info">
-          <span className="btn-title">Button</span>
-        </button>
+      <h1>Interpolation</h1>
     </div> 
   );
 }
